5888118d17df333fc9b007df10afeaa5e3d7bfd7
Vu Quoc Khanh <vuquockhanhhht@gmail.com>
2025-07-16 09:53:55 +0700

    Add mini.nvim

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,13 @@
+return {
+    'echasnovski/mini.nvim',
+    lazy = false, -- mini.nvim loads very fast
+    config = function()
+        -- mini.icons
+        local icons = require 'mini.icons'
+        icons.setup()
+        icons.mock_nvim_web_devicons()
+
+        -- mini.files
+        require('mini.files').setup()
+    end,
+}
