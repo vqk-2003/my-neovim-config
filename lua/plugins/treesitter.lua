8011f2f242e3f293e22219e2fc575799460dc974
Vu Quoc Khanh <vuquockhanhhht@gmail.com>
2025-07-16 18:05:55 +0700

    Add treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,33 @@
+return {
+    'nvim-treesitter/nvim-treesitter',
+    branch = 'master',
+    lazy = false,
+    build = ':TSUpdate',
+    main = 'nvim-treesitter.configs',
+    opts = {
+        ensure_installed = {
+            'bash',
+            'c',
+            'diff',
+            'html',
+            'lua',
+            'luadoc',
+            'markdown',
+            'markdown_inline',
+            'query',
+            'vim',
+            'vimdoc',
+        },
+        auto_install = true,
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = { 'ruby' },
+        },
+        indent = {
+            enable = true,
+            disable = {
+                'ruby',
+            },
+        },
+    },
+}
