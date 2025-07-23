76253ff6c6acee10cc24620fe4e042b595db69c6
Vu Quoc Khanh <vuquockhanhhht@gmail.com>
2025-07-23 22:43:24 +0700

    OK I think this is the base setting for my lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -23,6 +23,36 @@ return {
           vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
         end
 
+        local picker = require("snacks.picker")
+        -- Find references for the word under your cursor.
+        map("grr", picker.lsp_references, "[G]oto [R]eferences")
+
+        -- Jump to the implementation of the word under your cursor.
+        --  Useful when your language has ways of declaring types without an actual implementation.
+        map("gri", picker.lsp_implementations, "[G]oto [I]mplementation")
+
+        -- Jump to the definition of the word under your cursor.
+        --  This is where a variable was first declared, or where a function is defined, etc.
+        --  To jump back, press <C-t>.
+        map("grd", picker.lsp_definitions, "[G]oto [D]efinition")
+
+        -- WARN: This is not Goto Definition, this is Goto Declaration.
+        --  For example, in C this would take you to the header.
+        map("grD", picker.lsp_declarations, "[G]oto [D]eclaration")
+
+        -- Fuzzy find all the symbols in your current document.
+        --  Symbols are things like variables, functions, types, etc.
+        map("gO", picker.lsp_symbols, "Open Document Symbols")
+
+        -- Fuzzy find all the symbols in your current workspace.
+        --  Similar to document symbols, except searches over your entire project.
+        map("gW", picker.lsp_workspace_symbols, "Open Workspace Symbols")
+
+        -- Jump to the type of the word under your cursor.
+        --  Useful when you're not sure what type a variable is and you want to see
+        --  the definition of its *type*, not where it was *defined*.
+        map("grt", picker.lsp_type_definitions, "[G]oto [T]ype Definition")
+
         local client = vim.lsp.get_client_by_id(event.data.client_id)
 
         -- Auto-completion
